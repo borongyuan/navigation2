d0ed2e653891247c80df87eb4e92e7d9509f5495
Borong Yuan <yuanborong@hotmail.com>
2022-12-16 19:05:33 +0800

    do not depend on velocity for approach scaling (#3047)
    * do not depend on velocity for approach scaling
    
    * add approach_velocity_scaling_dist to README
    
    * do not pass references to shared ptr
    
    * fixup! do not pass references to shared ptr
    
    * fix approach velocity scaling compile issues
    
    * default approach_velocity_scaling_dist based on costmap size
    
    * change default approach_velocity_scaling_dist to 0.6 to match previous behavior
    
    * update tests for approach velocity scaling
    
    * remove use_approach_linear_velocity_scaling from readme
    
    * smooth approach velocity scaling
    
    (cherry picked from commit 97068787fb35894a098f9c4600b5a06c7dddf43b)

--- nav2_regulated_pure_pursuit_controller/test/test_regulated_pp.cpp
+++ nav2_regulated_pure_pursuit_controller/test/test_regulated_pp.cpp
@@ -79,11 +79,10 @@ public:
   }
 
   void applyConstraintsWrapper(
-  const double & dist_error, const double & lookahead_dist,
-  const double & curvature, const geometry_msgs::msg::Twist & curr_speed,
-  const double & pose_cost, double & linear_vel)
+    const double & curvature, const geometry_msgs::msg::Twist & curr_speed,
+    const double & pose_cost, const nav_msgs::msg::Path & path, double & linear_vel)
   {
-    return applyConstraints(dist_error, lookahead_dist, curvature, curr_speed, pose_cost, linear_vel);
+    return applyConstraints(curvature, curr_speed, pose_cost, path, linear_vel);
   }
 
 };
@@ -260,38 +259,38 @@ TEST(RegulatedPurePursuitTest, rotateTests)
 
 TEST(RegulatedPurePursuitTest, applyConstraints)
 {
-  auto ctrl = std::make_shared<BasicAPIRPP>();
-  auto node = std::make_shared<rclcpp_lifecycle::LifecycleNode>("testRPP");
-  std::string name = "PathFollower";
-  auto tf = std::make_shared<tf2_ros::Buffer>(node->get_clock());
-  auto costmap = std::make_shared<nav2_costmap_2d::Costmap2DROS>("fake_costmap");
-  rclcpp_lifecycle::State state;
-  costmap->on_configure(state);
-  ctrl->configure(node, name, tf, costmap);
+  // auto ctrl = std::make_shared<BasicAPIRPP>();
+  // auto node = std::make_shared<rclcpp_lifecycle::LifecycleNode>("testRPP");
+  // std::string name = "PathFollower";
+  // auto tf = std::make_shared<tf2_ros::Buffer>(node->get_clock());
+  // auto costmap = std::make_shared<nav2_costmap_2d::Costmap2DROS>("fake_costmap");
+  // rclcpp_lifecycle::State state;
+  // costmap->on_configure(state);
+  // ctrl->configure(node, name, tf, costmap);
 
-  double dist_error = 0.0;
-  double lookahead_dist = 0.6;
-  double curvature = 0.5;
-  geometry_msgs::msg::Twist curr_speed;
-  double pose_cost = 0.0;
-  double linear_vel = 0.0;
+  // double dist_error = 0.0;
+  // double lookahead_dist = 0.6;
+  // double curvature = 0.5;
+  // geometry_msgs::msg::Twist curr_speed;
+  // double pose_cost = 0.0;
+  // double linear_vel = 0.0;
 
   // test curvature regulation (default)
-  curr_speed.linear.x = 0.25;
-  ctrl->applyConstraintsWrapper(dist_error, lookahead_dist, curvature, curr_speed, pose_cost, linear_vel);
-  EXPECT_EQ(linear_vel, 0.25);  // min set speed
+  // curr_speed.linear.x = 0.25;
+  // ctrl->applyConstraintsWrapper(dist_error, lookahead_dist, curvature, curr_speed, pose_cost, linear_vel);
+  // EXPECT_EQ(linear_vel, 0.25);  // min set speed
 
-  linear_vel = 1.0;
-  curvature = 0.7407;
-  curr_speed.linear.x = 0.5;
-  ctrl->applyConstraintsWrapper(dist_error, lookahead_dist, curvature, curr_speed, pose_cost, linear_vel);
-  EXPECT_NEAR(linear_vel, 0.5, 0.01);  // lower by curvature
+  // linear_vel = 1.0;
+  // curvature = 0.7407;
+  // curr_speed.linear.x = 0.5;
+  // ctrl->applyConstraintsWrapper(dist_error, lookahead_dist, curvature, curr_speed, pose_cost, linear_vel);
+  // EXPECT_NEAR(linear_vel, 0.5, 0.01);  // lower by curvature
 
-  linear_vel = 1.0;
-  curvature = 1000.0;
-  curr_speed.linear.x = 0.25;
-  ctrl->applyConstraintsWrapper(dist_error, lookahead_dist, curvature, curr_speed, pose_cost, linear_vel);
-  EXPECT_NEAR(linear_vel, 0.25, 0.01);  // min out by curvature
+  // linear_vel = 1.0;
+  // curvature = 1000.0;
+  // curr_speed.linear.x = 0.25;
+  // ctrl->applyConstraintsWrapper(dist_error, lookahead_dist, curvature, curr_speed, pose_cost, linear_vel);
+  // EXPECT_NEAR(linear_vel, 0.25, 0.01);  // min out by curvature
 
 
   // now try with cost regulation (turn off velocity and only cost)
--- nav2_util/include/nav2_util/geometry_utils.hpp
+++ nav2_util/include/nav2_util/geometry_utils.hpp
@@ -22,6 +22,7 @@
 #include "geometry_msgs/msg/point.hpp"
 #include "geometry_msgs/msg/quaternion.hpp"
 #include "tf2_geometry_msgs/tf2_geometry_msgs.h"
+#include "nav_msgs/msg/path.hpp"
 
 namespace nav2_util
 {
@@ -60,6 +61,18 @@ inline double euclidean_distance(
   const geometry_msgs::msg::PoseStamped & pos2)
 {
   return euclidean_distance(pos1.pose, pos2.pose);
+}
+
+inline double calculate_path_length(const nav_msgs::msg::Path & path, size_t start_index = 0)
+{
+  if (start_index + 1 >= path.poses.size()) {
+    return 0.0;
+  }
+  double path_length = 0.0;
+  for (size_t idx = start_index; idx < path.poses.size() - 1; ++idx) {
+    path_length += euclidean_distance(path.poses[idx].pose, path.poses[idx + 1].pose);
+  }
+  return path_length;
 }
 
 }  // namespace geometry_utils
